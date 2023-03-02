79d981fdb99e2973f17f7db14926c48e3caa04d5
bensonaa1988 <56173851+bensonaa1988@users.noreply.github.com>
2023-03-02 15:48:53 -0800

    updates

--- app.py
+++ app.py
@@ -1,12 +1,4 @@
 import pandas as pd
-# import csv
-# with open('ClassDataWItemNum.csv', 'r') as file:
-#     reader = csv.reader(file)
-
-#     for line in reader:
-#         id = line[1]
-#         print(id[0:4])
-
     
 
 table = pd.read_csv("ClassDataWItemNum.csv")
